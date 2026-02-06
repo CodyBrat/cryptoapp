18bec1d419168630fcfd37708b826fbb45a2da42
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-06 12:50:18 +0530

    public assets

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
-  return (
-    <header>
-        <div className="main-container inner">
-            
-        </div>
-    </header>
-  )
+    return (
+        <header>
+            <div className="main-container inner">
+                <Link href="/">
+                    <Image src="/assets/logo.svg" alt="Logo" width={130} height={40} />
+                </Link>
+            </div>
+        </header>
+    )
 }
 
 export default Header

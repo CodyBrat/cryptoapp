a49accecf3551a0435631bbc656f4f5e447bcbc2
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-06 12:58:01 +0530

    feat: Implement client-side header navigation with active link styling and update homepage content to an empty div.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,24 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
+    const pathname = usePathname()
     return (
         <header>
             <div className="main-container inner">
                 <Link href="/">
                     <Image src="/assets/logo.svg" alt="Logo" width={130} height={40} />
                 </Link>
+                <nav>
+                    <Link href="/" className={cn("nav-link", { "is-active": pathname === "/", "is-home": true })}>Home</Link>
+                    <p>Search Modal</p>
+                    <Link href="/coins" className={cn("nav-link", { "is-active": pathname === "/coins" })}> All Coins</Link>
+                </nav>
             </div>
         </header>
     )

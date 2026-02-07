961197582c483bf14e1bc1cf728424a1907cb08e
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-07 20:32:54 +0530

    feat: Display top 3 coins as images in a new 'Top Gainers' column for categories, leveraging enhanced DataTable column styling.

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,12 +2,14 @@
 import { fetcher } from '@/lib/coingecko.actions'
 import React from 'react'
 import DataTable from '@/components/DataTable'
+import Image from 'next/image'
 
 
 const Categories = async () => {
     const categories = await fetcher<Category[]>(`/coins/categories`)
     const columns:DataTableColumn<Category>[]=[
-        {header:'Category',cellClassName:'category-cell',cell:(category)=>category.name}
+        {header:'Category',cellClassName:'category-cell',cell:(category)=>category.name},
+        {header:'Top Gainers',cellClassName:'top-gainers-cell',cell:(category)=>category.top_3_coins.map((coin)=>(<Image src={coin} alt={coin} key={coin} width={28} height={28}/>))}
     ]
   return (
     <div id='categories' className='custom-scrollbar'>
